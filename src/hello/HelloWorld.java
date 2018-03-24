5647c7b8c03df302366fe06e2a1d62aa30ce5fb8
YirguBira <yirgunig@gmail.com>
2018-03-23 22:48:29 -0400

    project1 setup

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class HelloWorld {
+
+        public HelloWorld(){
+
+        }
+        public void hello(){
+
+            System.out.println("Hello world!");
+        }
+}
---
+++ src/hello/TestHelloWorld.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class TestHelloWorld {
+
+    public static void main (String [] args){
+
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.hello();
+    }
+}
